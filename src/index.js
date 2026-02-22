b1ef04d71dde741286967d5d212aeb3f5bc047a3
Menacolato <menajosue7@gmail.com>
2026-02-21 20:59:57 -0800

    Update package-lock.json, package.json, and src/index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Educational Control running 🚀");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// health check
+app.get("/", (req, res) => res.send("Educational Control API running 🚀"));
+
+// ping de prueba
+app.get("/api/ping", (req, res) => res.json({ ok: true, message: "pong" }));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
