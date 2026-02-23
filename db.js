41105affe720fff6304e92dc01ffcf510bf17d84
Josue Mena <menajosue7@gmail.com>
2026-02-22 20:51:40 -0800

    Switch to MySQL connection pool with env vars
    Refactored database connection to use a connection pool with environment variables for configuration.

--- db.js
+++ db.js
@@ -1,18 +1,28 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "gaby1810",
-    database: "educational_control"
+const {
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PASS,
+} = process.env;
+
+const pool = mysql.createPool({
+  host: DB_HOST,
+  port: Number(DB_PORT || 3306),
+  user: DB_USER,
+  password: DB_PASSWORD || DB_PASS,
+  database: DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect(err => {
-    if (err) {
-        console.log("Error conexión:", err);
-    } else {
-        console.log("Conectado a MySQL");
-    }
-});
+console.log("DB_HOST:", DB_HOST);
+console.log("DB_PORT:", DB_PORT);
+console.log("DB_USER:", DB_USER);
+console.log("DB_NAME:", DB_NAME);
 
-module.exports = db;
+module.exports = pool;
