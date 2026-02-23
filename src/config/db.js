ef9e635e45b8968b517b6696a61eada394130ec9
Menacolato <menajosue7@gmail.com>
2026-02-22 16:10:12 -0800

    Aplicacnion de control educativo

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "Estudiante"
+    database: "Educational_Control"
 });
 
 connection.connect((err) => {
