3a04f3ebeecbd802e9ad5fd7a34fec9b4b7b5372
Menacolato <menajosue7@gmail.com>
2026-02-22 16:10:52 -0800

    Nombre del commit: Update database configuration

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "Educational_Control"
+    database: "educational_control"
 });
 
 connection.connect((err) => {
